b7232326c9544ae86fce3a6ecaad735b071b0ddc
blogup <73314290+blogup@users.noreply.github.com>
2021-07-17 08:24:37 -0300

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,14 +4,14 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB8s760YcuM7_H1orjfoDASmH31FOrPwCY",
-    authDomain: "letmeask-projeto-a2916.firebaseapp.com",
-    databaseURL: "https://letmeask-projeto-a2916-default-rtdb.firebaseio.com",
-    projectId: "letmeask-projeto-a2916",
-    storageBucket: "letmeask-projeto-a2916.appspot.com",
-    messagingSenderId: "230011109510",
-    appId: "1:230011109510:web:68f979985c181dc15bd107"
-  };
+  apiKey: "AIzaSyB8s760YcuM7_H1orjfoDASmH31FOrPwCY",
+  authDomain: "letmeask-projeto-a2916.firebaseapp.com",
+  databaseURL: "https://letmeask-projeto-a2916-default-rtdb.firebaseio.com",
+  projectId: "letmeask-projeto-a2916",
+  storageBucket: "letmeask-projeto-a2916.appspot.com",
+  messagingSenderId: "230011109510",
+  appId: "1:230011109510:web:68f979985c181dc15bd107"
+};
 
   firebase.initializeApp(firebaseConfig);
 
