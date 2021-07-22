7ccef65644f9a418ed319d2fb850139945486c0b
blogup <rodrigo.lima.devops@gmail.com>
2021-07-22 15:35:57 -0300

    farebase 1

---
+++ .history/src/services/firebase_20210722152440.ts
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: R,
+  authDomain: "letmeask-projeto-a2916.firebaseapp.com",
+  databaseURL: "https://letmeask-projeto-a2916-default-rtdb.firebaseio.com",
+  projectId: "letmeask-projeto-a2916",
+  storageBucket: "letmeask-projeto-a2916.appspot.com",
+  messagingSenderId: "230011109510",
+  appId: "1:230011109510:web:68f979985c181dc15bd107"
+};
+
+
+  firebase.initializeApp(firebaseConfig);
+
+  export const auth = firebase.auth();
+  export const database = firebase.database();
+
+  export {firebase}
--- .history/src/services/firebase_20210722152756.ts
+++ .history/src/services/firebase_20210722152756.ts
@@ -6,12 +6,11 @@ import 'firebase/database';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  databaseURL: process.env.,
+  projectId: process.env.,
+  storageBucket: process.env.,
+  messagingSenderId: process.env.,
+  appId: process.env.
 };
 
 
